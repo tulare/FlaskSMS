80703e86e53909e73ac9eca02666964a768393dd
tulare <tulare.paxgalactica@gmail.com>
2019-01-14 08:48:16 +0100

    Add pybluez python3.5 linux; fix environment variables

--- runserver.py
+++ runserver.py
@@ -1,14 +1,14 @@
 """
-This script runs the FlaskWebTrain application using a development server.
+This script runs the FlaskApp application using a development server.
 """
 
 from os import environ
 from FlaskApp import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('FLASK_RUN_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('FLASK_RUN_PORT', '5000'))
     except ValueError:
-        PORT = 5555
+        PORT = 5000
     app.run(HOST, PORT)
